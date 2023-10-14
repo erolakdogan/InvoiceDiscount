6cf653e6e6027d04f5c493b972a229fcff84cfa0
erolakdogan <erolakdogan78@gmail.com>
2023-10-15 02:59:20 +0300

    created diagram.

--- InvoiceDiscount.Application/Strategies/OldCustomerDiscountStrategy.cs
+++ InvoiceDiscount.Application/Strategies/OldCustomerDiscountStrategy.cs
@@ -1,11 +1,7 @@
 ﻿using InvoiceDiscount.Application.Interfaces;
 using InvoiceDiscount.Domain.Entities;
 using InvoiceDiscount.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace InvoiceDiscount.Application.Strategies
 {
